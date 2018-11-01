8836f72f8dc5b8d2314a75fe1276ad740df860ab
Jianbo He <hjbxyz@gmail.com>
2018-11-01 11:00:07 +0800

    Use netlib-lapack on Windows

--- install-centos.sh
+++ install-centos.sh
@@ -9,10 +9,10 @@ if [ $1 == "lnx64" ]; then
 
 elif [ $1 == "win32" ]; then
 
-    i686-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-assoc.exe -s -O2 -std=c++11 -static -lopenblas -lgfortran -lquadmath -fopenmp
+    i686-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-assoc.exe -s -O2 -std=c++11 -static -llapack -lrefblas -lgfortran -lquadmath -fopenmp
 
 elif [ $1 == "win64" ]; then
 
-    x86_64-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-assoc.exe -s -O2 -std=c++11 -static -lopenblas -lgfortran -lquadmath -fopenmp
+    x86_64-w64-mingw32-g++ *.cpp -o $1/rtm-gwas-assoc.exe -s -O2 -std=c++11 -static -llapack -lrefblas -lgfortran -lquadmath -fopenmp
 
 fi
