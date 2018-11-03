ea818cedc386a3277b04de2575aa9a8f96a0692c
Jianbo He <hjbxyz@gmail.com>
2018-11-03 16:38:52 +0800

    Add scripts for linking with MKL

---
+++ install-gcc-mkl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MKLROOT=/opt/intel/compilers_and_libraries/linux/mkl
+
+LIBMKL1=${MKLROOT}/lib/intel64/libmkl_intel_lp64.a
+LIBMKL2=${MKLROOT}/lib/intel64/libmkl_sequential.a
+LIBMKL3=${MKLROOT}/lib/intel64/libmkl_core.a
+
+g++ *.cpp -o rtm-gwas-assoc -s -O2 -std=c++11 -static -fopenmp \
+    -Wl,--start-group ${LIBMKL1} ${LIBMKL2} ${LIBMKL3} -Wl,--end-group \
+    -lpthread -lm -ldl
