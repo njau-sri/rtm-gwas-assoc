a1bbb40aebefbf11a21a6da23cbaa73799fb8286
Jianbo He <hjbreg@users.noreply.github.com>
2018-11-17 10:12:50 +0800

    Update and rename install-linux-mkl.sh to install-glnx64-mkl.sh

--- install-glnx64-mkl.sh
+++ install-glnx64-mkl.sh
@@ -6,9 +6,9 @@ LIBMKL1=${MKLROOT}/lib/intel64/libmkl_intel_lp64.a
 LIBMKL2=${MKLROOT}/lib/intel64/libmkl_sequential.a
 LIBMKL3=${MKLROOT}/lib/intel64/libmkl_core.a
 
-rm -rf lnx64
-mkdir lnx64
+rm -rf glnx64
+mkdir glnx64
 
-g++ *.cpp -o lnx64/rtm-gwas-assoc -s -O2 -std=c++11 -static -fopenmp \
+g++ *.cpp -o glnx64/rtm-gwas-assoc -s -O2 -std=c++11 -static -fopenmp \
     -Wl,--start-group ${LIBMKL1} ${LIBMKL2} ${LIBMKL3} -Wl,--end-group \
     -lpthread -lm -ldl
