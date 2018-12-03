771736790d8e276937b915ea2e02d7e23e91b806
Jianbo He <hjbxyz@gmail.com>
2018-12-03 12:21:56 +0800

    Updated version.h

--- rtm_gwas_assoc.cpp
+++ rtm_gwas_assoc.cpp
@@ -628,7 +628,7 @@ int fit_multi_locus_model(size_t t, const std::vector<size_t> &loc,
 
 int rtm_gwas_assoc(int argc, char *argv[])
 {
-    std::cerr << "RTM-GWAS " RTM_GWAS_VERSION " ASSOC (Built on " __DATE__ " " __TIME__ ")\n";
+    std::cerr << "RTM-GWAS " RTM_GWAS_VERSION_STRING " ASSOC (Built on " __DATE__ " " __TIME__ ")\n";
 
     CmdLine cmd;
 
