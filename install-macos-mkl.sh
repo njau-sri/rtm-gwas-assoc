33a466302cd41eb463f9eed7fc83127aa900db50
Jianbo He <hjbxyz@gmail.com>
2019-04-30 13:57:22 +0800

    Use clang on macOS

--- install-macos-mkl.sh
+++ install-macos-mkl.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-MKLROOT=/opt/intel/mkl
+export PATH="/usr/local/opt/llvm@7/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/llvm@7/lib"
+export CPPFLAGS="-I/usr/local/opt/llvm@7/include"
 
+MKLROOT=/opt/intel/mkl
 LIB1=$MKLROOT/lib/libmkl_intel_lp64.a
 LIB2=$MKLROOT/lib/libmkl_sequential.a
 LIB3=$MKLROOT/lib/libmkl_core.a
@@ -15,5 +18,5 @@ if [ -z "$RTM_GWAS_VERSION" ]; then
     RTM_GWAS_VERSION=unknown
 fi
 
-g++ src/*.cpp -o $TARGET -O2 -std=c++11 $LIB1 $LIB2 $LIB3 -lpthread -lm -ldl \
+clang++ src/*.cpp -o $TARGET -O2 -std=c++11 -fopenmp $LIB1 $LIB2 $LIB3 -lpthread -lm -ldl \
     -DRTM_GWAS_VERSION=$RTM_GWAS_VERSION
